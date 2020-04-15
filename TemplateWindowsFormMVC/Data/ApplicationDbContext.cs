eb734efa2334eb7ce8999eeabb9c437e2b72b264
Dimitar <dim_kolev2002@abv.bg>
2020-04-15 23:27:35 +0300

    creating project

---
+++ TemplateWindowsFormMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Data.Models
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Conection.ConnectionString).UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+
+        }
+    }
+}
---
+++ TemplateWindowsFormMVC/Data/Conection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public static class Conection
+    {
+        public static string ConnectionString = "Server=.\\SQLExpress;Database=TemplateWindowsFormMVC;Trusted_Connection=True;";
+    }
+}
